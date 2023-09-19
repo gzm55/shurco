8609583a99ca5118114f0e1e22394f0790ee7209
James Z.M. Gao <gaozm55@gmail.com>
2023-09-19 11:03:55 +0800

    fix warnings on gcc 7

--- build.sh
+++ build.sh
@@ -5,10 +5,10 @@ set -ex
 git submodule update --init
 
 gcc -Wall -Wextra -Werror -pedantic -O3 -c shurco.c
-gcc -Wall -Wextra -Werror -pedantic -O3 -c ext/shoco/shoco.c -o ./shoco.o
 
-# unixshox has some warnings
-gcc -Wall -Wextra -Werror -pedantic -Wno-strict-prototypes -Wno-sign-compare -O3 -c ext/unishox/unishox2.c -o ./unishox2.o
+# shoco and unixshox has some warnings
+gcc -Wall -Wextra -Werror -pedantic -Wno-overflow  -O3 -c ext/shoco/shoco.c -o ./shoco.o
+gcc -Wall -Wextra -Werror -pedantic -Wno-strict-prototypes -Wno-sign-compare -Wno-strict-overflow -O3 -c ext/unishox/unishox2.c -o ./unishox2.o
 
 gcc -Wall -Wextra -Werror -pedantic -O3 shurco.o shurco-test.c -o shurco-test
 gcc -Wall -Wextra -Werror -pedantic -O3 shurco.o shoco.o unishox2.o shurco-benchmark.c -o shurco-benchmark
